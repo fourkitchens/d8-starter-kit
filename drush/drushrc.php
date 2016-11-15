0dc11895ae12b986f89825761a530d8e12762937
Jeff Tomlinson <jeff@fourkitchens.com>
2016-11-15 11:57:29 -0700

    Drush shell aliases.

--- drush/drushrc.php
+++ drush/drushrc.php
@@ -1,24 +1,43 @@
 <?php
 
-$options['shell-aliases'] = [
-  'confex' =>
-    "!echo '\nDisabling development modules...'
-    drush {{@target}} pmu -y $(cat ../mods_enabled.local | tr '\n' ' ')
-    echo '\nExporting configuration...'
-    drush {{@target}} cex -y
-    echo '\nRe-enabling development modules...'
-    drush {{@target}} en -y $(cat ../mods_enabled.local | tr '\n' ' ')
-    echo '\nImporting development module configuration...'
-    drush @nyu-pubhealth.local cim local --partial -y",
-  'confim' =>
-    "!echo '\nInitial cache clear...'
-    drush {{@target}} cr
-    echo '\nImporting configuration...'
-    drush {{@target}} cim sync -y
-    echo '\nEnabling development modules...'
-    drush {{@target}} en -y $(cat ../mods_enabled.local | tr '\n' ' ')
-    echo '\nUpdating database...'
-    drush {{@target}} updb -y
-    echo '\nFinal cache clear...'
-    drush {{@target}} cr",
-];
+/**
+ * @file
+ * Drush runtime configuration.
+ *
+ * @see https://github.com/drush-ops/drush/blob/master/examples/example.drushrc.php
+ */
+
+$options['shell-aliases'] = [];
+
+// Configuration export.
+$options['shell-aliases']['confex'] = implode("\n", [
+  "!echo '\nDisabling development modules...'",
+  "drush pm-uninstall -y $(cat ../mods_enabled.local | tr '\n' ' ')",
+
+  "echo '\nExporting configuration...'",
+  "drush config-export sync -y",
+
+  "echo '\nRe-enabling development modules...'",
+  "drush pm-enable -y $(cat ../mods_enabled.local | tr '\n' ' ')",
+
+  "echo '\nImporting development module configurations...'",
+  "drush config-import local --partial -y",
+]);
+
+// Configuration import.
+$options['shell-aliases']['confim'] = implode("\n", array(
+  "!echo '\nInitial cache clear...'",
+  "drush cache-rebuild",
+
+  "echo '\nImporting configuration...'",
+  "drush config-import sync -y",
+
+  "echo '\nEnabling development modules...'",
+  "drush pm-enable -y $(cat ../mods_enabled.local | tr '\n' ' ')",
+
+  "echo '\nUpdating database...'",
+  "drush updatedb -y",
+
+  "echo '\nFinal cache clear...'",
+  "drush cache-rebuild",
+));
