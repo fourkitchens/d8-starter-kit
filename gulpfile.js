74e8ac793037750d642f575904445177a5d6a194
Jeff Tomlinson <jeff@fourkitchens.com>
2017-09-07 16:15:26 -0600

    Use project_gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,7 @@
 /* globals require */
 
 (function () {
-
   'use strict';
-
-  // General.
   let gulp = require('gulp-help')(require('gulp'));
-
-  // Project gulp tasks.
-  require('./gulp-tasks/project.js')(gulp);
-
-  // TODO: Fix code smell implementation
-  // See: https://github.com/fourkitchens/d8-starter-kit/issues/1
-
-  // Emulsify gulp tasks.
-  // let localConfig = {};
-  //
-  // try {
-  //   localConfig = require('./emulsify.gulp-config');
-  // }
-  // catch (e) {
-  //   // Do nothing.
-  // }
-  //
-  // require('emulsify-gulp')(gulp, localConfig);
-
+  require('project-gulp')(gulp, []);
 })();
